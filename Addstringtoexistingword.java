d9dddc2c60767f16d2ed74cb6b162a7557740505
Madusudhan Reddy <ymadusudhanreddy@paypal.com>
2018-02-15 16:29:59 +0530

    Addstringtoexistingword.java

---
+++ Addstringtoexistingword.java
@@ -0,0 +1,12 @@
+package stringspack;
+
+public class Addstringtoexistingword {
+
+	public static void main(String[] args) {
+		String s="The quick brown fox jumps over the lazy dog";
+		String s1="and killed it.";
+		System.out.println(s+" "+s1);
+		
+	}
+
+}
